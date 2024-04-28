e4d50ba1741b880e5386e1366fd1f8b9fa1d4e09
Tulsi Rai <trai@Tulsi-Rai-2.fios-router.home>
2024-04-27 18:18:23 -0700

    Initial Commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugins: ['react-native'],
+  rules: {
+    'no-trailing-spaces': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-native/no-inline-styles': 'on',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+  },
 };
